8525be6b385685f4b196b1d9a952b19cb7286ef0
Jing Feng <59783150+cloudy3@users.noreply.github.com>
2020-11-17 00:54:58 +0800

    Create StudentManager.java

---
+++ Control/StudentManager.java
@@ -0,0 +1,28 @@
+package Control;
+
+import Entities.Student;
+
+public class StudentManager {
+	private Student user;
+	
+	public StudentManager(Student user) {
+		this.user = user;
+	}
+	public void findIndex(int index) {
+		// Finds the course of the index
+		
+	}
+	public void addCourse(String courseCode) {
+		// Shows index in course
+		
+		// Student adds index
+		
+		// Appends to ArrayList
+	}
+	public void dropCourse(String courseCode) {
+		// Remove from ArrayList
+	}
+	public Student getUser() {
+		return user;
+	}
+}
