5cb308362afdc55b49a1ff4be768f5b57fcac63f
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2020-11-17 15:54:27 +0800

    Update Course.java

--- entities/Course.java
+++ entities/Course.java
@@ -15,56 +15,66 @@ public class Course {
 		this.courseCode = courseCode;
 		this.courseName = courseName;
 		this.school = school;
+		indexList = new ArrayList<Index> ();
 	}
 	
+	// ------ accessor methods ------
 	public String getCourseCode() {
 		return courseCode;
-	}
-	
-	public void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
 	}
 	
 	public String getCourseName() {
 		return courseName;
 	}
 
-	public void setCourseName(String courseName) {
-		this.courseName = courseName;
-	}
-
 	public String getSchool() {
 		return school;
-	}
-
-	public void setSchool(String school) {
-		this.school = school;
 	}
 
 	public int getAU() {
 		return AU;
 	}
 
-	public void setAU(int AU) {
-		this.AU = AU;
-	}
-
 	public ArrayList<Index> getIndexList() {
 		return indexList;
 	}
 
+
+	// ------ mutator methods ------
+	// set course code
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	
+	// set course name
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	// set the school offering the course
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	// set the number of AU for this course
+	public void setAU(int AU) {
+		this.AU = AU;
+	}
+	
+	// set the indexList to a predefined indexList
 	public void setIndexList(ArrayList<Index> indexList) {
 		this.indexList = indexList;
 	}
 	
+	// add an index to the indexList
 	public void addIndex(Index index) {
 		indexList.add(index);
 	}
 	
+	// remove an index from the indexList
 	public void removeIndex(Index index) {
 		if (indexList.contains(index)) {
 			indexList.remove(index);
 		}
 	}
-	
 }
