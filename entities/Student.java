f6ce963b27f27a411622e9aa1f285539991a8829
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2020-11-19 00:04:16 +0800

    added constructor used in db for Student

--- entities/Student.java
+++ entities/Student.java
@@ -14,6 +14,31 @@ public class Student extends User {
 	private ArrayList<RegisteredCourse> courseList; // list of courses Student is registered for
 	
 	// ------ constructor ------
+	// using this constructor for now
+	public Student(String username, String password, boolean adminAccess, String firstName, String lastName, String salt, String email,
+			String matricNumber, String gender, String nationality, int numAU) {
+		super(username, password, adminAccess, firstName, lastName, salt, email);
+		this.matricNumber = matricNumber;
+		this.gender = gender;
+		this.nationality = nationality;
+		this.numAU = numAU;
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR,2020);
+		cal.set(Calendar.MONTH,8);
+		cal.set(Calendar.DAY_OF_MONTH,11);
+		cal.set(Calendar.HOUR_OF_DAY,14);
+		cal.set(Calendar.MINUTE,0);
+		cal.set(Calendar.SECOND,0);
+		cal.set(Calendar.MILLISECOND,0);
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(cal.getTime());
+		cal2.set(Calendar.HOUR_OF_DAY,22);
+		cal.set(Calendar.DAY_OF_MONTH,14);
+		this.accessStart = cal;
+		this.accessEnd = cal2;
+		courseList = new ArrayList<RegisteredCourse>();
+	}
+	
 	public Student(String username, String password, boolean adminAccess, String firstName, String lastName, String salt, String email,
 					String matricNumber, String gender, String nationality, int numAU, Calendar accessStart, Calendar accessEnd) {
 		super(username, password, adminAccess, firstName, lastName, salt, email);
