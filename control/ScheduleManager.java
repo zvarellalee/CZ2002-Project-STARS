22aeb072de5b1f4702779527e04a61c8bd8f2b63
Jing Feng <59783150+cloudy3@users.noreply.github.com>
2020-11-19 11:15:55 +0800

    Update ScheduleManager.java

--- control/ScheduleManager.java
+++ control/ScheduleManager.java
@@ -3,7 +3,7 @@ package control;
 import java.util.Calendar;
 
 public class ScheduleManager {
-	public static boolean isOverlapping(Calendar testStart, Calendar testEnd, Calendar test2Start, Calendar test2End) {
-	    return testStart.before(test2End) && test2Start.before(testEnd);
+	public static boolean isOverlapping(Calendar periodStart, Calendar periodEnd, Calendar period2Start, Calendar period2End) {
+	    return periodStart.before(period2End) && period2Start.before(period2End);
 	}
 }
