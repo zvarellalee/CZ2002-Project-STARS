db9e3a0725cdea9ebcfc374cc9b1486840c5f989
Jing Feng <59783150+cloudy3@users.noreply.github.com>
2020-11-21 11:48:24 +0800

    Fixed Errors

--- boundary/StaffUI.java
+++ boundary/StaffUI.java
@@ -1,3 +1,10 @@
+/**
+ * User Interface for Staff
+ * @author Cheah Jing Feng
+ * @version 1.0
+ * @since 2020-11-20
+ */
+
 package boundary;
 
 import java.util.Calendar;
@@ -11,12 +18,23 @@ import control.StaffManager;
 
 public class StaffUI implements UserUI {
 	
+	/**
+	 * Stores which Staff is using the UI
+	 */
 	private static Staff user;
 	
+	/**
+	 * Gets the staff object
+	 * @return Staff object
+	 */
 	public Staff getStaff() {
 		return StaffUI.user;
 	}
 	
+	/**
+	 * Sets the current user to input Staff
+	 * @param user Staff
+	 */
 	public void setStaff(Staff user) {
 		StaffUI.user = user;
 	}
@@ -30,6 +48,9 @@ public class StaffUI implements UserUI {
 		staffUI.showUI();
 	}
 
+	/**
+	 * Displays the Staff User Interface
+	 */
 	public void showUI() {
 		
 		StaffManager staffManager = new StaffManager(user);
@@ -59,54 +80,36 @@ public class StaffUI implements UserUI {
 						// Choose which student to edit
 						String matric;
 						Student student;
+						boolean found = false;
 						
 						System.out.print("Enter Student's Matric Number to edit access period: ");
 						matric = sc.next();
 						// Find the student object using matriculation number
 						for (Student s : Database.studentList) {
-							if (s.getMatricNumber() == matric) {
+							if (s.getMatricNumber().equals(matric)) {
 								student = s;
 								
 								// Ask staff for access and end date and time
 								Calendar accessStart = Calendar.getInstance();
 								Calendar accessEnd = Calendar.getInstance();
-								int startMth, startDay, startHour, startMin, endMth, endDay, endHour, endMin;
-	
-								System.out.print("Enter Start Month (1-12): ");
-								startMth = (sc.nextInt()) - 1;
-								System.out.print("Enter Day: ");
-								startDay = sc.nextInt();
-								System.out.print("Enter Hour: ");
-								startHour = sc.nextInt();
-								System.out.print("Enter Minute: ");
-								startMin = sc.nextInt();
+								int startMth = -1, startDay = -1, startHour = -1, startMin = -1, endMth = -1, endDay = -1, endHour = -1, endMin = -1;
 								
-								accessStart.set(Calendar.MONTH, startMth);
-								accessStart.set(Calendar.DAY_OF_MONTH, startDay);
-								accessStart.set(Calendar.HOUR_OF_DAY, startHour);
-								accessStart.set(Calendar.MINUTE, startMin);
-								
-								System.out.print("Enter End Month (1-12): ");
-								endMth = (sc.nextInt()) - 1;
-								System.out.print("Enter Day: ");
-								endDay = sc.nextInt();
-								System.out.print("Enter Hour: ");
-								endHour = sc.nextInt();
-								System.out.print("Enter Minute: ");
-								endMin = sc.nextInt();
-								
-								accessEnd.set(Calendar.MONTH, endMth);
-								accessEnd.set(Calendar.DAY_OF_MONTH, endDay);
-								accessEnd.set(Calendar.HOUR_OF_DAY, endHour);
-								accessEnd.set(Calendar.MINUTE, endMin);
+								System.out.println("\n---Editing " + student.getFirstName() + " Access Start Period---");
+								accessStart = StaffUI.inputDate(startMth, startDay, startHour, startMin, accessStart, sc);
+								System.out.println("\n---Editing " + student.getFirstName() + " Access End Period---\n");
+								accessEnd = StaffUI.inputDate(endMth, endDay, endHour, endMin, accessEnd, sc);
 								
 								// Edit student access period
 								staffManager.editStudentAccessPeriod(accessStart, accessEnd, student);
-								System.out.println("Student's access period successfully changed!");
+								System.out.println("Student's access period successfully changed!\n");
+								found = true;
 								break;
 							}
 						}
-						System.out.println("Matriculation Number doesn not exist!\n");
+						// If student not found
+						if (found == false) {
+							System.out.println("Matriculation Number doesn not exist!\n");
+						}
 						break;
 					case 2:
 						// Add a student
@@ -142,27 +145,30 @@ public class StaffUI implements UserUI {
 							selection = sc.nextInt();
 							
 							if (selection == 1) {
-								System.out.print("Enter Course Code to add: ");
+								System.out.print("\nEnter Course Code to add: ");
 								String courseCode = sc.next();
-								System.out.print("Enter Course Name to add: ");
-								String courseName = sc.next();
-								System.out.print("Enter Faculty to add: ");
-								String school = sc.next();
-								System.out.print("Enter Number of AUs for the course: ");
+								String clear = sc.nextLine();
+								System.out.print("\nEnter Course Name to add: ");
+								String courseName = sc.nextLine();
+								System.out.print("\nEnter Faculty to add: ");
+								String school = sc.nextLine();
+								System.out.print("\nEnter Number of AUs for the course: ");
 								int au = sc.nextInt();
 								
 								staffManager.addCourse(courseCode, courseName, school, au);
 								Course newCourse = staffManager.findCourse(courseCode);
-								System.out.print("Enter number of Indexes to insert into Course (" + courseName + ") : ");
+								System.out.print("\nEnter number of Indexes to insert into Course (" + courseName + ") : ");
 								int numIndex = sc.nextInt();
 								for (int i = 0; i < numIndex; i++) {
-									System.out.print("Enter Index Number of Index: " + i);
+									System.out.println("Adding Index (" + i+1 + "/" + numIndex + ")");
+									System.out.print("\nEnter Index Number: ");
 									int index = sc.nextInt();
-									System.out.print("Enter Number of Vacancies for Index " + index + ": ");
+									System.out.print("\nEnter Number of Vacancies for Index " + index + ": ");
 									int vacancies = sc.nextInt();
 									Index newIndex = new Index(index, vacancies);
 									newCourse.addIndex(newIndex);
 								}
+								System.out.println("New Indexes Successfully Added!\n");
 							}
 							else if (selection == 2) {
 								System.out.print("Enter Course Code to update: ");
@@ -219,6 +225,62 @@ public class StaffUI implements UserUI {
 				sc.next();
 			}
 		} while (run);
+	}
+	
+	/**
+	 * Stores Calendar's Date from User Input
+	 * @param mth Month
+	 * @param day Day
+	 * @param hour Hour
+	 * @param min Minute
+	 * @param calendar Calendar Object
+	 * @param sc Scanner Object
+	 * @return Calendar Object
+	 */
+	private static Calendar inputDate(int mth, int day, int hour, int min, Calendar calendar, Scanner sc) {
+		try {			
+			while (true) {
+				// Input Month
+				System.out.print("Enter Month (1-12): ");
+				mth = (sc.nextInt()) - 1;
+				if (mth < 0 || mth > 11) {
+					System.out.println("Please enter an integer between 1 to 12! Please try again.");
+					mth = -1;
+					continue;
+				}
+				// Input Day
+				System.out.print("Enter Day: ");
+				day = sc.nextInt();
+				if (day < 1 || day > 31) {
+					System.out.println("Please a valid date! Please try again.");
+					continue;
+				}
+				// Input Hour
+				System.out.print("Enter Hour (24H format): ");
+				hour = sc.nextInt();
+				if (hour < 0 || hour > 23) {
+					System.out.println("Please input a valid time! Please try again.");
+					continue;
+				}
+				// Input Minute
+				System.out.print("Enter Minute: ");
+				min = sc.nextInt();
+				if (min < 0 || min > 59) {
+					System.out.println("Please enter a valid time! Please try again.");
+					continue;
+				}
+				
+				calendar.set(Calendar.MONTH, mth);
+				calendar.set(Calendar.DAY_OF_MONTH, day);
+				calendar.set(Calendar.HOUR_OF_DAY, hour);
+				calendar.set(Calendar.MINUTE, min);
+				return calendar;
+			}
+		}
+		catch (Exception InputMismatchException) {
+			System.out.println("Invalid input! Please try again.\n");
+		}
+		return calendar;
 	}
 	
 }
