66daceaaa24b7a0e9baae5e2e684d8650d3e44fa
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2020-11-21 22:09:42 +0800

    with Serialization functionality

--- src/control/LoginManager.java
+++ src/control/LoginManager.java
@@ -9,12 +9,29 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class LoginManager{
-	private User[] users;
+	private List<User> users;
+	
 	public LoginManager() {
-		// get user array from file?
+		users = new ArrayList<User>();
+		users.addAll(FileManager.read("student.dat"));
+		users.addAll(FileManager.read("staff.dat"));
 	}
 	
-	public UserUI authenticate(String username, String password) {
+	private void login(User user) {
+		if (user.getAdminAccess() == true) {
+			StaffUI newUI = new StaffUI(); // if admin, return staff UI
+			newUI.setStaff((Staff)user);
+			newUI.showUI();
+		} else {
+			if (LoginManager.checkAccessPeriod((Student)user) == true) {
+				StudentUI newUI = new StudentUI(); // else, return the student UI
+				newUI.setStudent((Student)user);
+				newUI.showUI();
+			}
+		}
+	}
+	
+	public boolean authenticate(String username, String password) {
 		User currUser = null;
 		for (User user: users) {
 			if (user.getUsername().contentEquals(username)) { // find the correct user given the username
@@ -22,31 +39,38 @@ public class LoginManager{
 			}
 		}
 		
-		if (currUser == null)
-			return null; // if user not found, return null
-		
-		String encryptedPassword = currUser.getPassword();
+		if (currUser == null) {
+			return false; // if user not found, return null
+		}
+
 		String salt = currUser.getSalt();
-		String decryptedPassword = getHashedPassword(encryptedPassword,salt);
-		if (password.equals(decryptedPassword)) {
-			if (currUser.getAdminAccess() == true) {
-				UserUI newUI = new StaffUI(); // if admin, return staff UI
-				return newUI;
-			} else {
-				if (LoginManager.checkAccessPeriod((Student)currUser) == true) {
-					UserUI newUI = new StudentUI(); // else, return the student UI
-					return newUI;
-				}
+		
+		if (salt == null) { // if the given password has no salt, create a new salt and hash the password
+			try {
+				salt = generateSalt(currUser.getPassword()); // get a new salt for the user
+				currUser.setSalt(salt);
+				String newPassword = getHashedPassword(currUser.getPassword(),salt);
+				currUser.setPassword(newPassword);
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
 			}
-		} 
-		return null;// if password is incorrect, return false
+		}
+		
+		String correctPassword = currUser.getPassword(); // password from user object
+		String hashPassword = getHashedPassword(password,salt); // password to compare
+		
+		if (hashPassword.equals(correctPassword)) {
+			login(currUser);
+			return true;
+		}
+		return false; // if password is incorrect, return false
 	}
 	
-	private static byte[] generateSalt(String password) throws NoSuchAlgorithmException {
+	private static String generateSalt(String password) throws NoSuchAlgorithmException {
 		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
 		byte[] salt = new byte[16];
 		sr.nextBytes(salt);
-		return salt;
+		return Base64.getEncoder().encodeToString(salt);
 	}
 	// generate the hashed password from the salt
 	private static String getHashedPassword(String passwordToHash, String salt) {
@@ -57,20 +81,16 @@ public class LoginManager{
 			byte[] passwordBytes = md.digest(passwordToHash.getBytes());
 			StringBuilder sb = new StringBuilder();
 			for(int i=0; i< passwordBytes.length ;i++)
-            {
-                sb.append(Integer.toString((passwordBytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            password = sb.toString();
+	           {
+	               sb.append(Integer.toString((passwordBytes[i] & 0xff) + 0x100, 16).substring(1));
+	           }
+	        password = sb.toString();
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 		return password;
 	}
-	
-	public void getFromFile() {
-		// get users array from file
-	}
-	
+
 	private static boolean checkAccessPeriod(Student stud) {
 		// Check current time
 		Calendar currentTime = Calendar.getInstance();
