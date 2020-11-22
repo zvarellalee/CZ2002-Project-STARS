b875376f8781f52ef66ba364522529afba2919f6
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2020-11-23 00:33:29 +0800

    added javadoc

--- src/entities/Course.java
+++ src/entities/Course.java
@@ -1,3 +1,12 @@
+/**
+ * Represents a course offered in the school.
+ * A course can have many indexes and 
+ * can be be registered by students.
+ * @author Stanley Ho
+ * @version 1.0
+ * @since 2020-11-20
+ */
+
 package entities;
 
 import java.io.Serializable;
@@ -5,13 +14,37 @@ import java.util.ArrayList;
 
 /* Each course refers to a particular module from a particular school.*/
 
+
 public class Course implements Serializable{
+	/**
+	 * Stores the Course Code of the Course 
+	 * Each Course is identified using the courseCode
+	 */
 	private String courseCode;
+	/**
+	 * Stores Name of the Course
+	 */
 	private String courseName;
+	/**
+	 * Stores the Faculty that offers the Course
+	 */
 	private String school;
+	/**
+	 * Stores the Number of Academic Units for the Course
+	 */
 	private int AU;
+	/**
+	 * Stores an Array List of Indexes in the Course
+	 */
 	private ArrayList<Index> indexList;
 
+	/**
+	 * Initializing the Course
+	 * @param courseCode Course Code
+	 * @param courseName Course Name
+	 * @param school Faculty
+	 * @param AU Number of Acadamic Units
+	 */
 	public Course(String courseCode, String courseName, String school, int AU) {
 		this.courseCode = courseCode;
 		this.courseName = courseName;
@@ -21,59 +54,100 @@ public class Course implements Serializable{
 	}
 	
 	// ------ accessor methods ------
+	/**
+	 * Gets the Course Code
+	 * @return courseCode Course Code
+	 */
 	public String getCourseCode() {
 		return courseCode;
 	}
 	
+	/**
+	 * Gets the Course Name
+	 * @return courseName Course Name
+	 */
 	public String getCourseName() {
 		return courseName;
 	}
 
+	/**
+	 * Gets the Faculty
+	 * @return school Faculty
+	 */
 	public String getSchool() {
 		return school;
 	}
 
+	/**
+	 * Gets the Number of Academic Units
+	 * @return AU Number of Academic Units
+	 */
 	public int getAU() {
 		return AU;
 	}
 
+	/**
+	 * Gets the array list of Indexes in the Course
+	 * @return indexList Array List of Indexes
+	 */
 	public ArrayList<Index> getIndexList() {
 		return indexList;
 	}
 
 
 	// ------ mutator methods ------
-	// set course code
+	/**
+	 * Sets the Course Code
+	 * @param courseCode Course Code
+	 */
 	public void setCourseCode(String courseCode) {
 		this.courseCode = courseCode;
 	}
 	
-	// set course name
+	/**
+	 * Sets the Course Name
+	 * @param courseName Course Name
+	 */
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
 	
-	// set the school offering the course
+	/**
+	 * Sets the Faculty offering the Course
+	 * @param school Faculty
+	 */
 	public void setSchool(String school) {
 		this.school = school;
 	}
 	
-	// set the number of AU for this course
+	/**
+	 * Sets the Number of Academic Units
+	 * @param AU Number of Academic Units
+	 */
 	public void setAU(int AU) {
 		this.AU = AU;
 	}
 	
-	// set the indexList to a predefined indexList
+	/**
+	 * Sets the array list of Indexes in the Course
+	 * @param indexList Array List of Indexes
+	 */
 	public void setIndexList(ArrayList<Index> indexList) {
 		this.indexList = indexList;
 	}
 	
-	// add an index to the indexList
+	/**
+	 * Adds an Index to the array list of Indexes in the Course
+	 * @param Index Object
+	 */
 	public void addIndex(Index index) {
 		indexList.add(index);
 	}
 	
-	// remove an index from the indexList
+	/**
+	 * Removes an Index from the array list of Indexes in the Course
+	 * @param Index Object
+	 */
 	public void removeIndex(Index index) {
 		if (indexList.contains(index)) {
 			indexList.remove(index);
