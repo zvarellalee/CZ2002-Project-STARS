9a2516ecf647f50612255697102ec9023700bf01
yoyobobs <63420530+yoyobobs@users.noreply.github.com>
2020-11-22 18:07:23 +0800

    Update StaffManager.java

--- src/control/StaffManager.java
+++ src/control/StaffManager.java
@@ -236,39 +236,4 @@ public class StaffManager extends Manager{
 		System.out.println();
 	}
 	
-	/**
-	/**
-	 * Finds the Course Object using the Course Code
-	 * @param courseCode Course Code
-	 * @return Course Object
-	 
-	public Course findCourse(String courseCode) {
-		for (Course c : Database.courseList) {
-			if (c.getCourseCode().equals(courseCode)) {
-				return c;
-			}
-		}
-		System.out.println("\nInvalid Course Code! Please try again.");
-		System.out.println("");
-		return null;
-	}
-	
-	/**
-	 * Finds the Index Object using the Index Number
-	 * @param Index Index Number
-	 * @return Index Object
-	 
-	public Index findIndex(int index) {
-		// Finds the Index object from index number
-		for (Course c : Database.courseList) {
-			for (Index i : c.getIndexList())
-				if (i.getIndexNumber() == index) {
-					return i;
-				}
-		}
-		System.out.println("\nInvalid Index! Please try again.");
-		System.out.println("");
-		return null;
-	}
-	*/
 }
