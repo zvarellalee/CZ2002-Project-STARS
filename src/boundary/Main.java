5acf75abc43d051ec8ec41037ef38b7113ab19b2
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2020-11-25 14:06:59 +0800

    clear dat files

--- src/boundary/Main.java
+++ src/boundary/Main.java
@@ -9,7 +9,7 @@ package boundary;
 public class Main {
 	public static void main(String[] args) {
 		// to initialise pre-loaded values
-		//Database.initialise(); 
+		Database.initialise(); 
 		UserUI login = new LoginUI();
 		login.showUI();
 	}
